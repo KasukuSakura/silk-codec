d44bee60b76be786283dec375bfc73a8ca56582c
Karlatemp <karlatemp@vip.qq.com>
2021-10-02 00:34:25 +0800

    1

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -6,7 +6,7 @@ cd cmake-build-release || exit 1
 
 cmake -DCMAKE_BUILD_TYPE=Release "-DCMAKE_CACHEFILE_DIR=$PWD" "-Dcross_triple=$CROSS" ..
 
-cmake --build . --target native -- -j 3
+cmake --build . --target silk -- -j 3
 cmake --build . --target pcm_to_silk -- -j 3
 
 ecode=$?
