8c61ae993304ea2489b69390093fb858bea52e79
Karlatemp <karlatemp@vip.qq.com>
2021-10-02 11:09:16 +0800

    45

--- ci-helper/src/main/java/ci/PNative.java
+++ ci-helper/src/main/java/ci/PNative.java
@@ -8,12 +8,17 @@ import java.nio.file.StandardCopyOption;
 @SuppressWarnings("ConstantConditions")
 public class PNative {
     private static void cp(File s, File t) throws IOException {
-        if (!s.isFile()) return;
+        if (!s.isFile()) {
+            System.out.println(s.getAbsolutePath() + " not exists");
+            return;
+        }
         t.getParentFile().mkdirs();
         Files.copy(s.toPath(), t.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        System.out.println("Copied " + s.getAbsolutePath() + " to " + t.getAbsolutePath());
     }
 
     public static void main(String[] args) throws Throwable {
+        System.out.println("Working dir: " + new File(".").getAbsolutePath());
         var artifacts = new File("tmp/artifacts");
         var rs = new File("src/main/resources/io/github/kasukusakura/silkcodec/natives");
         rs.mkdirs();
